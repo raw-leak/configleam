e4c9e4381e1afe7a7817fede0b9d2ea7d9212660
Mykhaylo Gusak <michaelxxi@outlook.es>
2024-01-24 19:25:29 +0100

    feat: added config loader :wrench:

---
+++ config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"log"
+	"sync"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Port  string `envconfig:"PORT"`
+	Token string `envconfig:"GIT_TOKEN"`
+	Url   string `envconfig:"GIT_URL"`
+}
+
+var (
+	config Config
+	once   sync.Once
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+}
+
+// Get reads config from environment. Once.
+func Get() *Config {
+	once.Do(func() {
+		err := envconfig.Process("", &config)
+		if err != nil {
+			log.Fatalf("Error processing config: %v", err)
+		}
+
+	})
+
+	return &config
+}
